9504e8b6e4b9ebeea289eb5aed60e661e6e37a5f
Copy05 <71971770+Copy05@users.noreply.github.com>
2024-02-14 10:29:22 +0100

    MergeInfo: Added a total count of all possible merges

--- scripts/DataLoader.js
+++ scripts/DataLoader.js
@@ -4,7 +4,16 @@ function loadData() {
         .then(data => {
             // Assign loaded merge rules to the mergeRules constant
             Object.assign(mergeRules, data);
-            console.log('Merge rules loaded:', mergeRules, `Total Merges: ${Object.keys(mergeRules).length}`);
+            console.log('Merge rules loaded:', mergeRules, `Total Mergable Elements: ${Object.keys(mergeRules).length}`);
+
+            // Calculate total number of possible merges
+            let totalMerges = 0;
+            Object.keys(mergeRules).forEach(key => {
+                const merges = mergeRules[key];
+                totalMerges += Object.keys(merges).length;
+            });
+
+            console.log('Total Merges Possible:', totalMerges);
         })
         .catch(error => {
             console.error('Error loading merge rules:', error);
